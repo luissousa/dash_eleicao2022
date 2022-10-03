7dd838ac1edf71069cbf801a3a7f8526b5b9aebe
Luis Antonio Almeida <luissousa@users.noreply.github.com>
2022-10-03 20:08:01 -0300

    novo modelo dash

--- dash_camp22.py
+++ dash_camp22.py
@@ -3,11 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+st.set_page_config(layout='wide',page_title='Eleições 2022 (CE-MA-PI)')
 st.title("Eleições 2022")
 st.markdown("Visões sobre as Eleições brasileiras ano:2022")
 candidatos_df = pd.read_csv('candidatos2022.csv',encoding='latin-1')
 
-seleciona_estado = st.selectbox('Qual estado você deseja vizualizar?',['Todos','CE','MA','PI'])
+seleciona_estado = st.sidebar.selectbox('Qual estado você deseja vizualizar?',['Todos','CE','MA','PI'])
 if seleciona_estado == 'CE':
     candidatos_df = candidatos_df[candidatos_df['SG_UF']=='CE']
 elif seleciona_estado == 'MA':
@@ -17,7 +18,7 @@ elif seleciona_estado == 'PI':
 else:
     pass
 
-sel_cargo = st.selectbox('Qual o campo pesquisado?',['GOVERNADOR','VICE-GOVERNADOR','DEPUTADO ESTADUAL','DEPUTADO FEDERAL','SENADOR','1º SUPLENTE','2º SUPLENTE'])
+sel_cargo = st.sidebar.selectbox('Qual o campo pesquisado?',['GOVERNADOR','VICE-GOVERNADOR','DEPUTADO ESTADUAL','DEPUTADO FEDERAL','SENADOR','1º SUPLENTE','2º SUPLENTE'])
 if sel_cargo == 'GOVERNADOR':
     candidatos_df = candidatos_df[candidatos_df['DS_CARGO']=='GOVERNADOR']
 elif sel_cargo == 'VICE-GOVERNADOR':
@@ -33,11 +34,21 @@ elif   sel_cargo == '1º SUPLENTE':
 else:
     candidatos_df = candidatos_df[candidatos_df['DS_CARGO']=='2º SUPLENTE']
 
-
-sns.set_theme(style="whitegrid")
+ax = sns.color_palette("pastel")
+ax = sns.set_theme(style='darkgrid', palette='pastel')
 fig, ax = plt.subplots()
+fig = plt.figure(figsize=(5,5))
 ax = sns.countplot(x=candidatos_df['DS_CARGO'],hue=candidatos_df['SG_UF'])
 for p in ax.patches:
-   ax.annotate('{}'.format(p.get_height()), (p.get_x()+0.12, p.get_height()+.05))
+    ax.annotate('{}'.format(p.get_height()), (p.get_x()+0.12, p.get_height()+.05))
+plt.title('Cargo')
 plt.xlabel(" ")
-st.pyplot(fig)
+plt.ylabel(" ")
+
+col1,col2,col3 = st.columns(3)
+with col1:
+    st.pyplot(fig)
+with col2:
+    st.title("XXXXXXXXXXXXXx")
+with col3:
+    st.title("XXXXXXXXXXXXXXx")
